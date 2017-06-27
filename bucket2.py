2e85f2053d11dcb5ceff081bbca9ba6f921d00cd
wickjohn2 <johnwick2@gmail.com>
2017-06-27 16:38:07 -0500

    Update and rename create_vm.py to bucket2.py

---
+++ bucket2.py
@@ -0,0 +1,20 @@
+URL_BASE = 'https://www.googleapis.com/'
+
+def GenerateConfig(context):
+
+  resources = [{
+      'type': 'storage.v1.bucket',
+      'name': context.env['deployment'],
+      'properties': {
+        'storageClass': context.properties['storageClass'],
+        'region': context.properties['region']
+      }
+    {
+      'type': 'storage.v1.bucketAccessControl'
+      'name': context.env['deployment'],
+      'properties': {
+        'acl': context.properties['acl']
+      }
+    }
+  }]
+  return {'resources': resources}
